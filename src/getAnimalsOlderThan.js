bb6595f0d185970055fb4685c37eaadd8a42da61
Karina Gonzalez <karinagonzalez99@gmail.com>
2022-11-03 10:37:56 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+const { species } = data;
+
+const getAnimalsOlderThan = (animal, age) => {
+  const bichinhu = species.find((index) => index.name === animal);
+  return bichinhu.residents.every((i) => i.age >= age);
+};
 
 module.exports = getAnimalsOlderThan;
