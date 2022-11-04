acb6ec8cfaa255fac2798f23a910f2a2daf04017
Karina Gonzalez <karinagonzalez99@gmail.com>
2022-11-04 20:50:31 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const adultos = entrants.filter((visi) =>
+    visi.age >= 18 && visi.age < 49.99);
+  const idosos = entrants.filter((visi) =>
+    visi.age >= 49.99);
+  const criancas = entrants.filter((visi) =>
+    visi.age < 18);
+
+  return { child: criancas.length, adult: adultos.length, senior: idosos.length };
 }
 
 function calculateEntry(entrants) {
-  // seu código
+  if (typeof entrants !== 'object') {
+    return 0;
+  } if (Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const entrada = countEntrants(entrants);
+  const qntidade = Object.entries(entrada);
+  return qntidade.reduce((total, [group, quantity]) => total + quantity * data.prices[group], 0);
 }
 
 module.exports = { calculateEntry, countEntrants };
