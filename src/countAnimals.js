c6cd3522ff85114159531c39b565bddf0d42b37b
Karina Gonzalez <karinagonzalez99@gmail.com>
2022-11-07 13:39:09 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const animais = {};
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    species.forEach((bichinhus) => { animais[bichinhus.name] = bichinhus.residents.length;
+    });
+    return animais;
+  }
+  if (!animal.sex) {
+    return species.find((bichuss) => bichuss.name === animal.specie).residents.length;
+  }
+  const animals = species.find((bichos) =>
+    bichos.name === animal.specie).residents.filter((moradores) =>
+    moradores.sex === animal.sex).length;
+  return animals;
 }
 
 module.exports = countAnimals;
