10ad0a4de27dadf663747e9c8ce6332698e4fea2
Karina Gonzalez <karinagonzalez99@gmail.com>
2022-11-07 21:24:52 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const { employees } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const bichinhus = employees.find((cuidador) =>
+    cuidador.id === id).responsibleFor[0];
+  const vovos = species.find((vovo) => vovo.id === bichinhus).residents.sort((index, i) =>
+    i.age - index.age);
+  return vovos.map((animaiszinhos) => Object.keys(animaiszinhos).map((key) => animaiszinhos[key]))[0];
 }
 
 module.exports = getOldestFromFirstSpecies;
